d84999012596bffc9baaaeaa4cac46458530e656
IsraelAraujo70 <israelaraujodeoliveira@gmail.com>
2024-10-22 14:07:27 -0300

    Componentizando as estruturas principais

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,13 @@
+<script setup>
+    const props = defineProps([
+        'tarefasPendentes'
+    ])
+</script>
+<template>
+    <header class="p-5 mb-4 mt-4 bg-light rounded-4">
+        <h1>Minhas tarefas</h1>
+        <p>
+            Você possui <span>{{ props.tarefasPendentes }}</span> tarefas pendentes
+        </p>
+    </header>
+</template>
